f1b5c3833ec57f408ca54cefd1b9444fb2e9ec8f
Kieran McDonald <kmcdonald@eclmining.com>
2020-11-02 16:57:53 -0700

    Minor cleanup stuff.

--- sdl_fun/netplay.h
+++ sdl_fun/netplay.h
@@ -28,7 +28,6 @@ struct PlayerConnectionInfo {
    int                  disconnect_start = 0;
 };
 
-//todo not sure where this belong right now
 //This is to transfer information from imgui window to the Player struct
 struct SessionInfo {
    bool host = false;
@@ -42,48 +41,51 @@ struct SessionInfo {
 };
 
 struct NetPlay {
-   GGPOSession* ggpo = nullptr;
+   GGPOSession* ggpo = nullptr;                           //Pointer to ggpo structure
 
-   GGPOPlayer players[GAME_MAX_PLAYERS];
-   GGPOPlayerHandle localPlayer = -1;
+   GGPOPlayer players[GAME_MAX_PLAYERS];                  //Structure used to create a GGPO session
+   GGPOPlayerHandle localPlayer = -1;                     //What is the GGPO player number for this player
 
-   PlayerConnectionInfo connections[GAME_MAX_PLAYERS];
-   int hostConnNum = -1;
-   int myConnNum = -1;
-   UserInput inputs[GAME_MAX_PLAYERS];
+   PlayerConnectionInfo connections[GAME_MAX_PLAYERS];    //Information about the connection status
+   int hostConnNum = -1;                                  //What is the connection number of the host (for connections)
+   int myConnNum = -1;                                    //What is my connection number (for connections)
+   UserInput inputs[GAME_MAX_PLAYERS];                    //Structure is populated with synchronized inputs every frame
 
-   SessionInfo hostSetup[GAME_MAX_PLAYERS];
-   int frameDelay[3] = { 2, 1, 10 };
-   int disconnectTime[3] = { 10000, 0, 30000 };
-   std::vector <std::string> messages;
-   bool upnp = false;
-   int timeSync = 0;
+   SessionInfo hostSetup[GAME_MAX_PLAYERS];               //Used to map connection info from UI to GGPO structs
+   int frameDelay[3] = { 2, 1, 10 };                      //How long should we delay the local user input in the session
+   int disconnectTime[3] = { 10000, 0, 30000 };           //How long do we wait before we disconnect a player (0 is forever)
+   std::vector <std::string> messages;                    //For network messages to user through UI
+   bool upnp = false;                                     //Is UPNP running... needed for port forwarding
+   int timeSync = 0;                                      //Used to delay updating the game if one player is too many frames ahead
 };
 
+//Server statuses for TCP game info transfer
 enum ServerStatus {
-   server_none = 0,  //Not running
-   server_started,  //Start socket creation
-   server_listening,  //Listening for incoming connections
+   server_none = 0,               //Not running
+   server_started,                //Start socket creation
+   server_listening,              //Listening for incoming connections
    server_accept,
    server_receive,
    server_send,
-   server_waiting,  //Waiting for user to hit START
+   server_waiting,                //Waiting for user to hit START
    server_ready,
-   server_done,  //Game setup is complete... ready for GGPO
+   server_done,                   //Game setup is complete... ready for GGPO
 };
 
+//Client status for TCP game info transfer
 enum ClientStatus {
-   client_none = 0,  //Not running
-   client_started,  //Start socket creation
-   client_connecting,  //Attempting connection
-   client_connected,  //Connected to the host
+   client_none = 0,               //Not running
+   client_started,                //Start socket creation
+   client_connecting,             //Attempting connection
+   client_connected,              //Connected to the host
    client_sent,
    client_received,
-   client_waiting,  //Waiting for input
-   client_loaded,  //Received game data and ready to play
+   client_waiting,                //Waiting for input
+   client_loaded,                 //Received game data and ready to play
    client_done,
 };
 
+//Has the socket successfully sent or received data
 enum SocketStatus {
    sock_none = 0,
    sock_sent,
@@ -91,12 +93,13 @@ enum SocketStatus {
    sock_ready,
 };
 
+//Socket structures for winsocks
 struct SocketInfo {
-   char name[20];
-   SOCKET sock = { 0 };
-   sockaddr_in address = { 0 };
-   char recBuff[BUFFERLEN];
-   SocketStatus status = sock_none;
+   char name[30];                        //Player name used to identify socket
+   SOCKET sock = { 0 };                  //Integer id for the socket
+   sockaddr_in address = { 0 };          //Information about the socket such as address and port
+   char recBuff[BUFFERLEN];              //Buffer used to receive messages
+   SocketStatus status = sock_none;      //Used to check if things were went or received successfully
 };
 
 ////Currently not used...
