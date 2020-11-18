bcbbe4d5960f08b52ae73cfa5ad5c30eb67ea670
Kieran McDonald <kmcdonald@eclmining.com>
2020-11-17 20:26:54 -0700

    Whoops. Assert on conflict with non-empty tile.

--- sdl_fun/board.cpp
+++ sdl_fun/board.cpp
@@ -857,7 +857,7 @@ void boardAssignSlot(Board* board, bool buffer = false) {
       col = t.xpos / board->tileWidth;
 
       Tile* current = boardGetTile(board, row, col);
-      if (current->type == tile_empty) { DebugBreak(); }  //This is a position conflict... bad
+      if (current->type != tile_empty) { DebugBreak(); }  //This is a position conflict... bad
       *current = t;
       tileSetTexture(board, current);
 
